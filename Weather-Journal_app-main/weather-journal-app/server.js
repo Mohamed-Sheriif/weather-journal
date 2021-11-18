9ba48036d1d512a044079fcb20c3d2ed8761ee5a
mohhamersheriif <50711675+mohhamersheriif@users.noreply.github.com>
2021-11-18 16:30:59 +0200

    Update server.js

--- Weather-Journal_app-main/weather-journal-app/server.js
+++ Weather-Journal_app-main/weather-journal-app/server.js
@@ -1,25 +1,24 @@
-// Empty JS dic to act as endpoint for all routes
+// Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Express to run server and routes */
+// Require Express to run server and routes
 const express = require('express');
-
-// Start up an instance of app 
+// Start up an instance of app
 const app = express();
+/* Middleware*/
+const bodyParser = require('body-parser');
 
-/* Dependencies */
-const bodyParser = require('body-parser')
-// Middleware
+//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
-
-/* Initialize the main project folder*/
+// Initialize the main project folder
 app.use(express.static('website'));
 
-const port = 3030;
-// Spin up the server
+const port = 8000;
+// Setup Server
 const server = app.listen(port, listening);
  
 function listening(){
@@ -27,18 +26,15 @@ function listening(){
     console.log(`running on localhost: ${port}`);
   };
 
+//Get Route
+app.get('/getting' , dataSending);
+//callback function
+function dataSending(req,res){
+    res.send(projectData);
+}
 
- // Post data to an array
-  app.post('/postData', addData);
-
-  function addData (req,res){
-      console.log(req);
-      projectData=req.body;
-  };
-
-  app.get('/allData', sendServerData);
-
-  function sendServerData (request, response) {
-    response.send(projectData);
-  
-  };
+//Post Route
+app.post('/posting' , function (req , res){
+    projectData = req.body;
+    res.send(projectData);
+})
